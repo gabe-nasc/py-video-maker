fd6c929b202ec683fcb75e808f7244c84a1e093b
Gabriel Nascimento <gabriel.amorim.nascimento@gmail.com>
2019-03-15 13:21:48 -0300

    Add the function to break the content into understandable sentences

--- text.py
+++ text.py
@@ -1,11 +1,12 @@
 from nltk import sent_tokenize
+import pprint as pp
 import Algorithmia
 import re
 
 def fetch_wikipedia_article(content):
     client = Algorithmia.client('simAoS+NCBot8SACwnHwpY0ezGR1')   
     algo = client.algo('web/WikipediaParser/0.1.2')  
-    content['source_original_content'] = algo.pipe(content['search_term']).result["content"]
+    content["source_original_content"] = algo.pipe(content['search_term']).result["content"]
     return content
 
 # Removes blank lines, dates in paretheses, and whatever is left of Markdown
@@ -18,10 +19,19 @@ def sanitize_content(content):
     # Removes dates in parentheses
     article = re.sub("/\((?:\([^()]*\)|[^()])*\)/gm", '', article)
 
-    content['source_original_content'] = article
+    content["source_sanitized_content"] = article
+    return content
+
+# Breaks the content into understandable sentences
+def break_into_sentences(content):
+    content["sentences"] = []
+    
+    for sentence in sent_tokenize(content["source_sanitized_content"]):
+        content["sentences"].append({'text':sentence, 'keywords':[], 'images':[]})
+    
     return content
 
 def text(content):
     content = fetch_wikipedia_article(content)
     content = sanitize_content(content)
-
+    content = break_into_sentences(content)
